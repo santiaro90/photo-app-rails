f4980cc7e6a8cf881e60cea0335e0563cc6e9eae
Santiago Alvarez Rodriguez <santiaro90@gmail.com>
2018-06-21 08:12:02 -0500

    Add bootstrap helper. Add navigation bar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
 module ApplicationHelper
+  def bootstrap_flash
+    messages = flash.map do |type, msg|
+      content_tag :div, msg, class: bootstrap_flash_class(type), role: 'alert'
+    end
+
+    messages.join("\n").html_safe
+  end
+
+  def bootstrap_flash_class(type)
+    type_class = case type
+                 when 'notice', 'success' then 'success'
+                 when 'alert', 'error' then 'danger'
+                 end
+
+    "alert alert-#{type_class}"
+  end
 end
